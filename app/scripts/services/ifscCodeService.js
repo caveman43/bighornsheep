dd3602c48ed15278fda1b0e8c0fc48218e4425c4
Pradeep Dewda <dewdap@dewdap-mbpr.local>
2015-08-13 22:27:38 -0700

    changing localhost to node server host domain name

--- app/scripts/services/ifscCodeService.js
+++ app/scripts/services/ifscCodeService.js
@@ -4,42 +4,42 @@
 
 angular.module('bighornsheepApp').factory('ifscCodeService',function($http){
     var factory = {};
-    var bighornsheepAppHost = '';
+    var bighornsheepAppHost = 'http://bighornsheep-tradewithme.rhcloud.com';
 
     factory.getBankDetailByIFSCcode = function(ifsc_code) {
         return 'data/data.json';
     };
 
     factory.getIFSCCodesBySearchText = function(query) {
-        return $http.get('http://localhost:3000/query?str='+query)
+        return $http.get(bighornsheepAppHost+'/query?str='+query)
             .then(function(res){
                return res.data;
             });
     };
 
     factory.greetUser = function(name){
-      return $http.get('http://localhost:3000/greet?name='+name)
+      return $http.get(bighornsheepAppHost+'/greet?name='+name)
           .then(function(res){
                 return res.data;
           });
     };
 
     factory.getStateList = function() {
-       return $http.get('http://localhost:3000/states')
+       return $http.get(bighornsheepAppHost+'/states')
            .then(function(res){
                 return res.data;
            });
     };
 
     factory.getBanksList = function() {
-       return $http.get('http://localhost:3000/banks')
+       return $http.get(bighornsheepAppHost+'/banks')
            .then(function(res){
                 return res.data;
            });
     };
 
     factory.getCityList = function(state) {
-       return $http.get('http://localhost:3000/cities?state='+state)
+       return $http.get(bighornsheepAppHost+'/cities?state='+state)
            .then(function(res){
                 return res.data;
            });
